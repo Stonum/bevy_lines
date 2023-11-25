a4016206e95ea728c726584591467fa4acb0a9e8
Arshinov_AN <lexxarsh@gmail.com>
2023-11-25 18:23:27 +0300

    build: deploy wasm game

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,12 @@ fn main() {
                 primary_window: Some(Window {
                     title: "Lines".into(),
                     resolution: (GameOptions::WINDOW_WIDTH, GameOptions::WINDOW_HEIGHT).into(),
+                    // Bind to canvas included in `index.html`
+                    canvas: Some("#bevy".to_owned()),
+                    // The canvas size is constrained in index.html and build/web/styles.css
+                    fit_canvas_to_parent: true,
+                    // Tells wasm not to override default event handling, like F5 and Ctrl+R
+                    prevent_default_event_handling: false,
                     ..default()
                 }),
                 ..default()
